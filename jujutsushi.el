9d1e7a5b05415a633ffde3b6716649f6cc471095
Javier Olaechea <pirata@gmail.com>
2024-03-25 09:21:11 -0500

    wip

--- jujutsushi.el
+++ jujutsushi.el
@@ -2,9 +2,21 @@
 
 ;;; Commentary
 
+;;; Code:
+
+(require 'cl-lib)
+(require 'project)
+
 ;;;###autoload
 (defun jj-describe ()
   ;; Use with-editor. call jj describe
   )
+
+;;;###autoload
+(defun jj-status ()
+  ""
+  (interactive)
+  )
+
 
 (provide 'jujutsushi)
