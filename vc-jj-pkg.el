89ef098b6c250e34ccf564ab4539b4838d164e19
Allen Li <darkfeline@felesatra.moe>
2025-01-24 01:59:14 +0000

    Remove package file
    Let package-vc generate it

--- vc-jj-pkg.el
+++
@@ -1,6 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "vc-jj" "0.1.0"
-  "Teach vc about jujutsu projects."
-  '((emacs "29.3"))
-  :homepage "https://github.com/darkfeline/jujutsushi"
-  :keywords '("vc" "tools"))
