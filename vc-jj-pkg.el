681242d3d6008eaf9d01e5c8547bc012ef9cbe46
Allen Li <darkfeline@felesatra.moe>
2025-01-24 01:29:21 +0000

    Update homepage

--- vc-jj-pkg.el
+++ vc-jj-pkg.el
@@ -1,5 +1,5 @@
 (define-package "vc-jj" "0.0.1"
   "Teach vc about jujutsu projects."
   '((emacs "29.3"))
-  :homepage ""
+  :homepage "https://github.com/darkfeline/jujutsushi"
   :keywords '("vc" "tools"))
