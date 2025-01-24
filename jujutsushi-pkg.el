a7a0457558d2e19434fe3766f90bdbd1fb64639b
Allen Li <darkfeline@felesatra.moe>
2025-01-24 01:24:10 +0000

    Normalize formatting

--- jujutsushi-pkg.el
+++ jujutsushi-pkg.el
@@ -1,7 +1,7 @@
 (define-package "jujutsushi" "0.0.1"
   "Bring jj into your domain expansion."
-  '((emacs  "29.3")
+  '((emacs "29.3")
     (with-editor "3.3.4")
-    (project   "0.11.1"))
+    (project "0.11.1"))
   :homepage "https://git.sr.ht/~puercopop/jujutsushi"
   :keywords '("vc" "tools"))
