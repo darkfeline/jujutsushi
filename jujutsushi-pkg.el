5ad3b3692e72d5bca03c0ac7849f1b05e699bf90
Allen Li <darkfeline@felesatra.moe>
2025-01-24 01:21:46 +0000

    Clean up repo to ship vc-jj standalone

--- jujutsushi-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "jujutsushi" "0.0.1"
-  "Bring jj into your domain expansion."
-  '((emacs  "29.3")
-    (with-editor "3.3.4")
-    (project   "0.11.1"))
-  :homepage "https://git.sr.ht/~puercopop/jujutsushi"
-  :keywords '("vcs" "tools"))
