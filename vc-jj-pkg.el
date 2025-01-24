8b52e573fad31ce5783af72e3a3439a2d82b160b
Allen Li <darkfeline@felesatra.moe>
2025-01-24 01:53:19 +0000

    Clean up package file

--- vc-jj-pkg.el
+++ vc-jj-pkg.el
@@ -1,4 +1,5 @@
-(define-package "vc-jj" "0.0.1"
+;;; -*- no-byte-compile: t -*-
+(define-package "vc-jj" "0.1.0"
   "Teach vc about jujutsu projects."
   '((emacs "29.3"))
   :homepage "https://github.com/darkfeline/jujutsushi"
